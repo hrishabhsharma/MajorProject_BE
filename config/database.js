2f44c7ca70e81dddeb9e03a8c23b503b0cfeecdb
Hrishabh-Inventurs <rishabh@inventurs.in>
2023-12-04 17:22:21 +0530

    update

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+mongoose.set('strictQuery', false);
+
 const connect = async () => {
   try {
     await mongoose.connect(process.env.DB_URL);
